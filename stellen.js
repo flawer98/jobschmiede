f0812a961b999cc2b1bb4dd0e3b978d6d9df8e16
flawer98 <123422993+flawer98@users.noreply.github.com>
2025-11-09 17:11:18 +0100

    Update stellen.js

--- stellen.js
+++ stellen.js
@@ -729,11 +729,9 @@
           `<td><span class='ba-chip'>${escapeXML(transferLabel)}</span></td>` +
           `<td><span class=\"ba-chip ${statusClass}\">${escapeXML(statusLabel)}</span></td>` +
           `<td class=\"ba-table__actions\">` +
-          `<div class=\"ba-table__action-group\">` +
           `<button type=\"button\" class=\"ba-btn ba-btn--ghost ba-btn--icon js-edit-item\" data-id=\"${escapeXML(id)}\" ${actionDisabled}>
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
           </button>` +
-          `</div>` +
           `</td>` +
           `</tr>`
         );
